5ab4c4b644cd15ab17570b784b049feefd67427c
Nathalia Trazzi <81270839+miucciaknows@users.noreply.github.com>
2024-02-08 23:21:57 -0300

    Update ClosetViewController.swift

--- MakeUpCloset/MakeUpCloset/ClosetViewController.swift
+++ MakeUpCloset/MakeUpCloset/ClosetViewController.swift
@@ -9,56 +9,73 @@
 import Foundation
 import UIKit
 
-struct Item {
+
+struct SubItem {
     var name: String
     var brand: String
 }
 
+struct Item {
+    var name: String
+    var brand: String
+    var subItems: [SubItem]
+}
+
 class ClosetViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    
     var selectedButton: ButtonType?
-    var itemsToShow: [String] = []
+    var itemsToShow: [Item] = []
     var expandedIndexes: Set<Int> = []
     var cellHeights: [CGFloat] = []
-    
+       
     @IBOutlet weak var tableContentView: UITableView!
     
     @IBOutlet weak var pageLabel: UILabel!
     
-    
-      var makeupItems: [Item] = []
-      var skincareItems: [Item] = []
-      var haircareItems: [Item] = []
-      var fragranceItems: [Item] = []
-    
       override func viewDidLoad() {
           super.viewDidLoad()
-          
           customizeTableView()
-          
           switch selectedButton {
           case .make_Up:
               pageLabel.text = "MakeUp"
-              itemsToShow = ["Lipstick", "Eyeshadow", "Eyelashes", "Foundation", "Mascara", "Blush"]
-              makeupItems = itemsToShow.map { Item(name: $0, brand: "") }
+              itemsToShow = [
+                  Item(name: "Lipstick", brand: "", subItems: []),
+                  Item(name: "Eyeshadow", brand: "", subItems: []),
+                  Item(name: "Eyelashes", brand: "", subItems: []),
+                  Item(name: "Foundation", brand: "", subItems: []),
+                  Item(name: "Mascara", brand: "", subItems: []),
+                  Item(name: "Blush", brand: "", subItems: [])
+              ]
           case .skin_care:
               pageLabel.text = "Skincare"
-              itemsToShow = ["Cleanser", "Moisturizer", "Sunscreen", "Serum", "Toner", "Exfoliator"]
-              skincareItems = itemsToShow.map { Item(name: $0, brand: "") }
+              itemsToShow = [
+                  Item(name: "Cleanser", brand: "", subItems: []),
+                  Item(name: "Moisturizer", brand: "", subItems: []),
+                  Item(name: "Sunscreen", brand: "", subItems: []),
+                  Item(name: "Serum", brand: "", subItems: []),
+                  Item(name: "Toner", brand: "", subItems: []),
+                  Item(name: "Exfoliator", brand: "", subItems: [])
+              ]
           case .hair_Care:
               pageLabel.text = "Haircare"
-              itemsToShow = ["Shampoo", "Conditioner", "Oil", "Mask", "Serum", "Styling Tools"]
-              haircareItems = itemsToShow.map { Item(name: $0, brand: "") }
+              itemsToShow = [
+                  Item(name: "Shampoo", brand: "", subItems: []),
+                  Item(name: "Conditioner", brand: "", subItems: []),
+                  Item(name: "Oil", brand: "", subItems: []),
+                  Item(name: "Mask", brand: "", subItems: []),
+                  Item(name: "Serum", brand: "", subItems: []),
+                  Item(name: "Styling Tools", brand: "", subItems: [])
+              ]
           case .fragrances:
               pageLabel.text = "Fragrances"
-              itemsToShow = ["Eau de Toilette", "Eau de Parfum"]
-              fragranceItems = itemsToShow.map { Item(name: $0, brand: "") }
+              itemsToShow = [
+                  Item(name: "Eau de Toilette", brand: "", subItems: []),
+                  Item(name: "Eau de Parfum", brand: "", subItems: [])
+              ]
           case nil:
               break
           }
           
-     
           for _ in itemsToShow {
               cellHeights.append(UITableView.automaticDimension)
           }
@@ -81,17 +98,43 @@ class ClosetViewController: UIViewController, UITableViewDataSource, UITableView
       
       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
           let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-          let itemName = itemsToShow[indexPath.row]
-          cell.textLabel?.text = itemName
+          let item = itemsToShow[indexPath.row]
           
-
           if expandedIndexes.contains(indexPath.row) {
-              cellHeights[indexPath.row] = 100
-          } else {
+              var text = item.name + "\n"
+              for subItem in item.subItems {
+                  text += "\(subItem.name) - \(subItem.brand)\n"
+              }
+              cell.textLabel?.numberOfLines = 0
+              cell.textLabel?.text = text
               cellHeights[indexPath.row] = UITableView.automaticDimension
+              // Adicionando botão "+" à célula expandida
+              let addButton = UIButton(type: .system)
+              addButton.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+              addButton.tintColor = .systemPink
+              addButton.addTarget(self, action: #selector(addSubitem(_:)), for: .touchUpInside)
+              addButton.frame = CGRect(x: 10, y: 70, width: 30, height: 30)
+              cell.accessoryView = addButton
+          } else {
+              cell.textLabel?.numberOfLines = 1
+              cell.textLabel?.text = item.name
+              cellHeights[indexPath.row] = UITableView.automaticDimension
+              cell.accessoryView = nil
           }
           
           return cell
+      }
+      
+      @objc func addSubitem(_ sender: UIButton) {
+          guard let tappedCell = sender.superview as? UITableViewCell,
+                let indexPath = tableContentView.indexPath(for: tappedCell) else {
+              return
+          }
+          // Adicionando um novo subitem à célula selecionada
+          let newItem = SubItem(name: "New Item", brand: "New brand")
+          itemsToShow[indexPath.row].subItems.append(newItem)
+          
+          tableContentView.reloadData()
       }
       
       func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -100,14 +143,11 @@ class ClosetViewController: UIViewController, UITableViewDataSource, UITableView
       
       func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
           tableView.deselectRow(at: indexPath, animated: true)
-          
-   
           if expandedIndexes.contains(indexPath.row) {
               expandedIndexes.remove(indexPath.row)
           } else {
               expandedIndexes.insert(indexPath.row)
           }
-          
           tableView.reloadRows(at: [indexPath], with: .automatic)
       }
   }
